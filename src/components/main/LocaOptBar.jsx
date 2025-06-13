1dd2b55338ce54a999490c6118cfe673ba857422
jhcodeDev <jhcode.dev@gmail.com>
2025-06-13 16:34:59 +0900

    loca section ongoing

--- src/components/main/LocaOptBar.jsx
+++ src/components/main/LocaOptBar.jsx
@@ -1,18 +1,24 @@
-import { useState, useRef } from 'react'
+import { useState, useRef } from 'react';
+import { useNavigate } from "react-router-dom";
 import "../css/LocaOptBar.css";
 
 
 const mockData = [
-  { id: 1, label: "donggu", img: "../../assets/logo_donggu.jpg" },
-  { id: 2, label: "dalseo", img: "../../assets/react.svg" },
-  { id: 3, label: "loca me2", img: "/img3.jpg" },
-  { id: 4, label: "loca me3", img: "../assets/react.svg" },
+  { id: "27290", label: "동구", img: "/assets/logo_donggu.jpg" },
+  { id: "27710", label: "달서구", img: "../../assets/react.svg" },
+  { id: "27170", label: "달성군", img: "/img3.jpg" },
+  { id: "27140", label: "북구", img: "../assets/react.svg" },
+  { id: "27230", label: "서구", img: "../assets/react.svg" },
+  { id: "27110", label: "중구", img: "../assets/react.svg" },
+  { id: "27260", label: "수성구", img: "../assets/react.svg" },
+  { id: "27200", label: "남구", img: "../assets/react.svg" },
   // 필요한 만큼 더 추가
 ];
 
 export default function LocaOptBar() {
   const [selectedId, setSelectedId] = useState(1);
   const scrollRef = useRef(null);
+  const navigate = useNavigate();
 
   const scroll = (direction) => {
     const scrollAmount = 200;
@@ -24,6 +30,16 @@ export default function LocaOptBar() {
     }
   };
 
+  const handleLoca = (selId) => {
+    setSelectedId(selId);
+    const params = new URLSearchParams({
+      selId,
+    });
+    console.log("locaCd: ", selId);
+
+    navigate(`/result?${params.toString()}`);
+  };
+
   return (
     <div className="loca-opt-bar">
       <button className="arrow-btn" onClick={() => scroll("left")}>◀</button>
@@ -33,9 +49,9 @@ export default function LocaOptBar() {
           <div
             key={item.id}
             className={`card ${selectedId === item.id ? "selected" : ""}`}
-            onClick={() => setSelectedId(item.id)}
+            onClick={() => handleLoca(item.id)}
           >
-            <img src={item.img} alt={item.label} />
+            {/* <img src={item.img} alt={item.label} /> */}
             <span>{item.label}</span>
             {selectedId === item.id && <div className="pin-icon">📍</div>}
           </div>
--- src/components/pages/DetailMapPage.jsx
+++ src/components/pages/DetailMapPage.jsx
@@ -5,25 +5,52 @@ import { useSearchParams, useNavigate } from "react-router-dom";
 function DetailMapPage() {
   const [searchParams] = useSearchParams();
   const [results, setResults] = useState([]);
+  const [filterText, setFilterText] = useState([]);
 
   const keyword = searchParams.get("keyword");
   const region = searchParams.get("region");
   const category = searchParams.get("category");
   const delivery = searchParams.get("delivery");
+  const gunguCd = searchParams.get("selId");
 
   const PUBLIC_API_KEY = import.meta.env.VITE_PUBLIC_API_KEY;
   const KAKAO_API_KEY = import.meta.env.VITE_KAKAO_API_KEY;
 
+  const filterData = () => {
+    //results
+    // const filteredData = results.filter(data => data.gunguCode.includes(gunguCd));
+    const stored = JSON.parse(localStorage.getItem('storeData')) || [];
+    // const filtered = stored.filter(item => item.storeName?.includes(keyword) || item.gunguCode?.includes(gunguCd));
+    const cleanKeyword = gunguCd.toString().trim();
+    const filtered = stored.filter(item => item.coJobSt==="01" && item.gunguCode?.toString().includes(cleanKeyword));
+
+    // stored.forEach(item => {
+    //   console.log(`item.gunguCode: [${item.gunguCode}], keyword: [${gunguCd}]`);
+    // });
+
+    console.log('filtered: ', filtered);
+    setFilterText(filtered);
+  };
+
   useEffect(() => {
     const fetchData = async () => {
+      const existing = localStorage.getItem('storeData');
+      if(existing) return;
+
       try {
         // ✅ 1. 공공 API 호출
         // const response = await fetch(`https://https://apis.data.go.kr/6270000/dgMealCardShop/getShopList?keyword=${keyword}&region=${region}&category=${category}&delivery=${delivery}`);
-        const response = await fetch(`https://apis.data.go.kr/6270000/dgMealCardShop/getShopList?serviceKey=${PUBLIC_API_KEY}&type=json&numOfRows=10&pageNo=1`);
+        const response = await fetch(`https://apis.data.go.kr/6270000/dgMealCardShop/getShopList?serviceKey=${PUBLIC_API_KEY}&type=json&numOfRows=5000&pageNo=1`);
 
         const data = await response.json();
-        setResults(data); // 응답 형식에 맞게 수정 필요
-        console.log(data);
+        const item = data.body.items.item;
+        // const item = json?.body?.items?.item || [];
+        localStorage.setItem('storeData', JSON.stringify(item));
+        localStorage.setItem('storeData_un', item);
+
+        console.log('API data sotred to localStorage');
+        setResults(item); // 응답 형식에 맞게 수정 필요
+        filterData();
       } catch (error) {
         console.error("API 요청 실패:", error);
       }
@@ -89,6 +116,13 @@ function DetailMapPage() {
         <p>배달 여부: {delivery}</p>
       </div>
       <div id="map" style={{ width: '540px', height: '500px' }}></div>
+      <div>
+        <ul>
+          {filterText.map((item, i) => (
+            <li key={i}>{item.shopName} - {item.gunguCode}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
